351df65918c244d19fd06672a225812ff1c53711
Greg Perlman <gperlman27@gmail.com>
2019-07-26 19:51:31 -0400

    move local static server to its own file

--- clients/mobile/App.tsx
+++ clients/mobile/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, RefObject, useRef, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,23 +10,15 @@ import {
   Button
 } from "react-native";
 import { WebView } from "react-native-webview";
-import StaticServer from "react-native-static-server";
-import RNFS from "react-native-fs";
 import Markdown from "react-native-simple-markdown";
 import { WebViewMessage } from "react-native-webview/lib/WebViewTypes";
 import { iOSUIKit } from "react-native-typography";
-
-let path = RNFS.MainBundlePath + "/build";
-let server = new StaticServer(8080, path, { localOnly: true });
-
-server.start().then((url: string) => {
-  console.log("Serving at URL", url);
-});
+import { useLocalServer } from "./src/lib/localStaticServer";
 
 const App = () => {
-  const webviewRef = useRef<WebView>();
+  const { uri } = useLocalServer();
   const [value, setValue] = useState("");
-  const [showMarkdownPreview, setShowMarkdownPreview] = useState(false);
+  const [showMarkdownPreview, setShowMarkdownPreview] = useState(true);
 
   const onMessage = (webviewEvent: NativeSyntheticEvent<WebViewMessage>) => {
     const decodedMessage: { event: string; value: string } = JSON.parse(
@@ -39,16 +31,20 @@ const App = () => {
 
     if (decodedMessage.event === "toggle_preview") {
       setShowMarkdownPreview(!showMarkdownPreview);
-
-      if (webviewRef.current) {
-        webviewRef.current.forceUpdate();
-      }
     }
   };
 
+  if (!uri) {
+    return (
+      <SafeAreaView style={styles.safeAreaView}>
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <Fragment>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.safeAreaView}>
         <StatusBar barStyle="dark-content" />
         <View>
           <Text>Top UI Bar Here</Text>
@@ -60,14 +56,13 @@ const App = () => {
         <View style={styles.container}>
           <View style={styles.webViewContainer}>
             <WebView
-              ref={webviewRef as RefObject<WebView>}
               keyboardDisplayRequiresUserAction={false}
               scrollEnabled={false}
               showsHorizontalScrollIndicator={false}
               showsVerticalScrollIndicator={false}
               bounces={false}
               overScrollMode="never"
-              source={{ uri: "http://localhost:8080" }}
+              source={{ uri }}
               javaScriptEnabled={true}
               originWhitelist={["*"]}
               allowFileAccess={true}
@@ -99,14 +94,17 @@ const styles = StyleSheet.create({
   },
   markdownContainer: {
     flex: 1
+  },
+  safeAreaView: {
+    flex: 1
   }
 });
 
 const markdownStyles = StyleSheet.create({
   heading1: {
     ...iOSUIKit.largeTitleEmphasizedObject,
-    marginBottom: iOSUIKit.largeTitleEmphasizedObject.fontSize * 0.3,
-    marginTop: iOSUIKit.largeTitleEmphasizedObject.fontSize * 0.3,
+    marginBottom: iOSUIKit.largeTitleEmphasizedObject.fontSize! * 0.3,
+    marginTop: iOSUIKit.largeTitleEmphasizedObject.fontSize! * 0.3
   },
   heading2: iOSUIKit.title3EmphasizedObject,
   heading3: iOSUIKit.title3Object,
---
+++ clients/mobile/src/lib/localStaticServer.ts
@@ -0,0 +1,52 @@
+import RNFS from "react-native-fs";
+import StaticServer from "react-native-static-server";
+import { useEffect, useState } from "react";
+
+interface StaticServer {
+  run(): Promise<string | void>;
+  stop(): void;
+  isRunning(): boolean;
+}
+
+const PORT = 8080;
+
+class LocalStaticServer implements StaticServer {
+  private port = PORT;
+  private path = RNFS.MainBundlePath + "/build";
+  private server = new StaticServer(this.port, this.path, { localOnly: true });
+
+  public async run(): Promise<string> {
+    return this.server.start();
+  }
+
+  public stop(): void {
+    this.server.stop();
+  }
+
+  public isRunning(): boolean {
+    return this.server.isRunning();
+  }
+}
+
+export const useLocalServer = () => {
+  const [uri, setUri] = useState<string | undefined>(undefined);
+  const server = new LocalStaticServer();
+
+  useEffect(() => {
+    console.log("this ran");
+
+    server
+      .run()
+      .then((url: string) => setUri(url))
+      .catch(e => {
+        throw e;
+      });
+
+    return () => server.stop();
+  }, []);
+
+  return {
+    uri,
+    setUri
+  };
+};
