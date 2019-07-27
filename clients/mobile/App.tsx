61b1adf72078f0fe3243588f4bd2853406f74dd8
Greg Perlman <gperlman27@gmail.com>
2019-07-26 20:27:01 -0400

    main layout ui logic uses useReducer

--- clients/mobile/App.tsx
+++ clients/mobile/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState, useReducer } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,10 +15,67 @@ import { WebViewMessage } from "react-native-webview/lib/WebViewTypes";
 import { iOSUIKit } from "react-native-typography";
 import { useLocalServer } from "./src/lib/localStaticServer";
 
+enum ActionType {
+  SHOW_PREVIEW_ONLY,
+  SHOW_EDITOR_ONLY,
+  SHOW_BOTH,
+  TOGGLE_SHOW_EDITOR,
+  TOGGLE_SHOW_PREVIEW
+}
+
+interface Action<T extends {} | ((...params: any) => any) = {}> {
+  type: ActionType;
+  payload?: T;
+}
+
+interface State {
+  showEditor: boolean;
+  showMarkdownPreview: boolean;
+}
+
+const initialState: State = {
+  showEditor: true,
+  showMarkdownPreview: true
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case ActionType.SHOW_PREVIEW_ONLY:
+      return {
+        showEditor: false,
+        showMarkdownPreview: true
+      };
+    case ActionType.SHOW_EDITOR_ONLY:
+      return {
+        showEditor: true,
+        showMarkdownPreview: false
+      };
+    case ActionType.SHOW_BOTH:
+      return {
+        showEditor: true,
+        showMarkdownPreview: true
+      };
+    case ActionType.TOGGLE_SHOW_EDITOR:
+      return {
+        ...state,
+        showEditor: !state.showEditor
+      };
+    case ActionType.TOGGLE_SHOW_PREVIEW:
+      return {
+        ...state,
+        showMarkdownPreview: !state.showMarkdownPreview
+      };
+    default:
+      console.log("Improper type dispatched, restting");
+
+      return initialState;
+  }
+};
+
 const App = () => {
   const { uri } = useLocalServer();
   const [value, setValue] = useState("");
-  const [showMarkdownPreview, setShowMarkdownPreview] = useState(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const onMessage = (webviewEvent: NativeSyntheticEvent<WebViewMessage>) => {
     const decodedMessage: { event: string; value: string } = JSON.parse(
@@ -30,7 +87,7 @@ const App = () => {
     }
 
     if (decodedMessage.event === "toggle_preview") {
-      setShowMarkdownPreview(!showMarkdownPreview);
+      dispatch({ type: ActionType.TOGGLE_SHOW_PREVIEW });
     }
   };
 
@@ -48,29 +105,41 @@ const App = () => {
         <StatusBar barStyle="dark-content" />
         <View>
           <Text>Top UI Bar Here</Text>
-          <Button
-            title={"click me"}
-            onPress={() => setShowMarkdownPreview(!showMarkdownPreview)}
-          />
-        </View>
-        <View style={styles.container}>
-          <View style={styles.webViewContainer}>
-            <WebView
-              keyboardDisplayRequiresUserAction={false}
-              scrollEnabled={false}
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}
-              bounces={false}
-              overScrollMode="never"
-              source={{ uri }}
-              javaScriptEnabled={true}
-              originWhitelist={["*"]}
-              allowFileAccess={true}
-              useWebKit={true}
-              onMessage={onMessage}
+          <View>
+            <Button
+              title={"Show Editor Only"}
+              onPress={() => dispatch({ type: ActionType.SHOW_EDITOR_ONLY })}
+            />
+            <Button
+              title={"Show Both"}
+              onPress={() => dispatch({ type: ActionType.SHOW_BOTH })}
+            />
+            <Button
+              title={"Preview Only"}
+              onPress={() => dispatch({ type: ActionType.SHOW_PREVIEW_ONLY })}
             />
           </View>
-          {showMarkdownPreview && (
+        </View>
+        <View style={styles.container}>
+          {state.showEditor && (
+            <View style={styles.webViewContainer}>
+              <WebView
+                keyboardDisplayRequiresUserAction={false}
+                scrollEnabled={false}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                bounces={false}
+                overScrollMode="never"
+                source={{ uri }}
+                javaScriptEnabled={true}
+                originWhitelist={["*"]}
+                allowFileAccess={true}
+                useWebKit={true}
+                onMessage={onMessage}
+              />
+            </View>
+          )}
+          {state.showMarkdownPreview && (
             <View style={styles.markdownContainer}>
               <ScrollView>
                 <Markdown styles={markdownStyles}>{value}</Markdown>
